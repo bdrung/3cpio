5871fb61922c0b6480666d450500cce531ad5fef
Benjamin Drung <bdrung@posteo.de>
2024-06-26 13:29:44 +0200

    Drop assert_cmd and predicates dev dependencies
    For simplifying packaging 3cpio for the Ubuntu main archive, drop the
    dev dependencies `assert_cmd` and `predicates`. Implement the needed
    functionality by just using functions from the Rust standard library.

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,87 +1,170 @@
 // Copyright (C) 2024, Benjamin Drung <bdrung@posteo.de>
 // SPDX-License-Identifier: ISC
 
+use std::env;
 use std::error::Error;
-use std::process::Command;
+use std::process::{Command, Output};
 
-use assert_cmd::prelude::*;
-use predicates::prelude::*;
+// Derive target directory (e.g. `target/debug`) from current executable
+fn get_target_dir() -> std::path::PathBuf {
+    let mut path = env::current_exe().expect("env::current_exe not set");
+    path.pop();
+    if path.ends_with("deps") {
+        path.pop();
+    }
+    path
+}
+
+fn get_command() -> Command {
+    let mut program = get_target_dir();
+    program.push("3cpio");
+    Command::new(program)
+}
+
+trait ExitCodeAssertion {
+    fn assert_failure(self, expected_code: i32) -> Self;
+    fn assert_success(self) -> Self;
+}
+
+impl ExitCodeAssertion for Output {
+    fn assert_failure(self, expected_code: i32) -> Self {
+        assert_eq!(self.status.code().expect("exit code"), expected_code);
+        self
+    }
+
+    fn assert_success(self) -> Self {
+        assert!(self.status.success());
+        self
+    }
+}
+
+trait OutputAssertion<S> {
+    fn assert_stderr(self, expected: S) -> Self;
+    fn assert_stdout(self, expected: S) -> Self;
+}
+
+impl<S> OutputAssertion<S> for Output
+where
+    String: PartialEq<S>,
+    S: std::fmt::Debug,
+{
+    fn assert_stderr(self, expected: S) -> Self {
+        let stderr = String::from_utf8(self.stderr.clone()).expect("stderr");
+        assert_eq!(stderr, expected);
+        self
+    }
+
+    fn assert_stdout(self, expected: S) -> Self {
+        let stdout = String::from_utf8(self.stdout.clone()).expect("stdout");
+        assert_eq!(stdout, expected);
+        self
+    }
+}
+
+trait OutputContainsAssertion {
+    fn assert_stderr_contains(self, expected: &str) -> Self;
+}
+
+impl OutputContainsAssertion for Output {
+    fn assert_stderr_contains(self, expected: &str) -> Self {
+        let stderr = String::from_utf8(self.stderr.clone()).expect("stderr");
+        assert!(
+            stderr.contains(expected),
+            "'{}' not found in '{}'",
+            expected,
+            stderr
+        );
+        self
+    }
+}
 
 #[test]
 fn examine_compressed_cpio() -> Result<(), Box<dyn Error>> {
     for compression in ["bzip2", "gzip", "lz4", "lzop", "xz", "zstd"] {
-        let mut cmd = Command::cargo_bin("3cpio")?;
+        let mut cmd = get_command();
         cmd.arg("-e").arg(format!("tests/{}.cpio", compression));
 
-        cmd.assert()
-            .success()
-            .stdout(format!("0\tcpio\n512\t{}\n", compression));
+        cmd.output()?
+            .assert_stderr("")
+            .assert_success()
+            .assert_stdout(format!("0\tcpio\n512\t{}\n", compression));
     }
     Ok(())
 }
 
 #[test]
 fn examine_single_cpio() -> Result<(), Box<dyn Error>> {
-    let mut cmd = Command::cargo_bin("3cpio")?;
+    let mut cmd = get_command();
     cmd.arg("-e").arg("tests/single.cpio");
 
-    cmd.assert().success().stdout("0\tcpio\n");
+    cmd.output()?.assert_success().assert_stdout("0\tcpio\n");
     Ok(())
 }
 
 #[test]
 fn file_doesnt_exist() -> Result<(), Box<dyn Error>> {
-    let mut cmd = Command::cargo_bin("3cpio")?;
+    let mut cmd = get_command();
     cmd.arg("-t").arg("test/file/does/not/exist");
 
-    cmd.assert()
-        .failure()
-        .code(1)
-        .stderr(predicate::str::contains("No such file or directory"));
+    cmd.output()?
+        .assert_failure(1)
+        .assert_stderr_contains("No such file or directory")
+        .assert_stdout("");
     Ok(())
 }
 
 #[test]
 fn list_content_compressed_cpio() -> Result<(), Box<dyn Error>> {
     for compression in ["bzip2", "gzip", "lz4", "lzop", "xz", "zstd"] {
-        let mut cmd = Command::cargo_bin("3cpio")?;
+        let mut cmd = get_command();
         cmd.arg("-t").arg(format!("tests/{}.cpio", compression));
 
-        cmd.assert()
-            .success()
-            .stdout(".\npath\npath/file\n.\nusr\nusr/bin\nusr/bin/sh\n");
+        cmd.output()?
+            .assert_stderr("")
+            .assert_success()
+            .assert_stdout(".\npath\npath/file\n.\nusr\nusr/bin\nusr/bin/sh\n");
     }
     Ok(())
 }
 
 #[test]
 fn list_content_single_cpio() -> Result<(), Box<dyn Error>> {
-    let mut cmd = Command::cargo_bin("3cpio")?;
+    let mut cmd = get_command();
     cmd.arg("-t").arg("tests/single.cpio");
 
-    cmd.assert().success().stdout(".\npath\npath/file\n");
+    cmd.output()?
+        .assert_stderr("")
+        .assert_success()
+        .assert_stdout(".\npath\npath/file\n");
     Ok(())
 }
 
 #[test]
 fn missing_file_argument() -> Result<(), Box<dyn Error>> {
-    let mut cmd = Command::cargo_bin("3cpio")?;
+    let mut cmd = get_command();
     cmd.arg("-t");
 
-    cmd.assert()
-        .failure()
-        .code(2)
-        .stderr(predicate::str::contains("missing argument FILE"));
+    cmd.output()?
+        .assert_failure(2)
+        .assert_stderr_contains("missing argument FILE")
+        .assert_stdout("");
     Ok(())
 }
 
 #[test]
 fn print_version() -> Result<(), Box<dyn Error>> {
-    let mut cmd = Command::cargo_bin("3cpio")?;
+    let mut cmd = get_command();
     cmd.arg("--version");
 
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::is_match("^[a-z]+ [0-9.]+\n$").unwrap());
+    let stdout = cmd.output()?.assert_stderr("").assert_success().stdout;
+    let stdout = String::from_utf8(stdout).expect("stdout");
+    let words: Vec<&str> = stdout.split_whitespace().collect();
+    assert_eq!(words.len(), 2, "not two words: '{}'", stdout);
+
+    let version = words[1];
+    // Simple implementation for regular expression match: [0-9.]+
+    let mut matches = String::from(version);
+    matches.retain(|c| c.is_ascii_digit() || c == '.');
+    assert_eq!(matches, version);
     Ok(())
 }
