3e2da23c54bf9bf6214f06ff41e824626b84a500
Benjamin Drung <bdrung@posteo.de>
2025-08-09 13:55:01 +0200

    doc: add documentation for TempDir

--- src/temp_dir.rs
+++ src/temp_dir.rs
@@ -7,16 +7,45 @@ use std::path::PathBuf;
 use std::time::SystemTime;
 
 pub struct TempDir {
+    /// Path of the temporary directory.
     pub path: PathBuf,
     cwd: Option<PathBuf>,
 }
 
 impl TempDir {
+    /// Creates a new temporary directory.
+    ///
+    /// This temporary directory and all the files it contains will be removed
+    /// on drop.
+    ///
+    /// **Warning**: The temporary directory is constructed by using `CARGO_PKG_NAME`
+    /// plus the UNIX timestamp in nanoseconds. This directory name could be guessed
+    /// and result in "insecure temporary file" security vulnerabilities. Therefore
+    /// this function is only meant for being use in test cases.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use threecpio::temp_dir::TempDir;
+    ///
+    /// let tempdir = TempDir::new().unwrap();
+    /// println!("Temporary directory: {}", tempdir.path.display());
+    /// ```
     pub fn new() -> Result<Self> {
         let path = create_tempdir()?;
         Ok(Self { path, cwd: None })
     }
 
+    /// Creates a new temporary directory and changes the current working
+    /// directory to this directory.
+    ///
+    /// This temporary directory and all the files it contains will be removed
+    /// on drop. The current working directory will be set back on drop as well.
+    ///
+    /// **Warning**: The temporary directory is constructed by using `CARGO_PKG_NAME`
+    /// plus the UNIX timestamp in nanoseconds. This directory name could be guessed
+    /// and result in "insecure temporary file" security vulnerabilities. Therefore
+    /// this function is only meant for being use in test cases.
     pub fn new_and_set_current_dir() -> Result<Self> {
         let path = create_tempdir()?;
         let cwd = current_dir()?;
@@ -29,6 +58,7 @@ impl TempDir {
 }
 
 impl Drop for TempDir {
+    /// Removes the temporary directory and all the files it contains.
     fn drop(&mut self) {
         if let Some(cwd) = self.cwd.as_ref() {
             let _ = set_current_dir(cwd);
